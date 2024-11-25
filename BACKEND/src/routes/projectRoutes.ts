ef650576e38157e15fb259953a281190aa7f46d3
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-11-25 17:08:11 -0500

    449. Agregando validacion en el Servidor a los proyectos

--- BACKEND/src/routes/projectRoutes.ts
+++ BACKEND/src/routes/projectRoutes.ts
@@ -1,9 +1,20 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { ProjectController } from "../controllers/ProjectController";
+import { handleInpoutErrors } from "../middleware/validation";
 
 const router = Router();
 
-router.post('/', ProjectController.createProject);
+router.post('/', 
+    body('projectName')
+        .notEmpty().withMessage('El Nombre del Projecto es Obligatorio'),
+    body('clientName')
+        .notEmpty().withMessage('El Nombre del Cliente es Obligatorio'),
+    body('description')
+        .notEmpty().withMessage('La Descripción del Proyecto es Obligatoria'),
+    handleInpoutErrors, // Si para la validación pasa al controlador, de lo contrario se de tiene la ejecución en el middleware
+    ProjectController.createProject
+);
 router.get('/', ProjectController.getAllProjects);
 
 export default router;
