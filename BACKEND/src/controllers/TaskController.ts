0bd1690854a2886dd7c4ad44208896295f2aa978
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-11-30 17:26:31 -0500

    461. Mejorando el Performance de nuestro con Promise.allSettled

--- BACKEND/src/controllers/TaskController.ts
+++ BACKEND/src/controllers/TaskController.ts
@@ -7,8 +7,9 @@ export class TaskController {
             const task = new Task(req.body); // Se crea un nuevo objeto Task con los datos del body
             task.project = req.project.id; // Se establece la relación entre la tarea y el proyecto
             req.project.tasks.push(task.id); // Se agrega el ID de la tarea al array de tareas del proyecto
-            await task.save(); // Se guarda el nuevo Task en la base de datos
-            await req.project.save(); // Se guarda el proyecto actualizado en la base de datos
+            // await task.save(); // Se guarda la tarea en la base de datos
+            // await req.project.save(); // Se guarda el proyecto en la base de datos
+            await Promise.allSettled([task.save(), req.project.save()]); // Se guardan ambos documentos en la base de datos - Mejora en performance y en orden
             res.send('Tarea creada correctamente');
         } catch (error) {
             console.log(error);
