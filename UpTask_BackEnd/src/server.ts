6724fd05f8c45baa2efafbad99e5966b0ab8e16c
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-03 11:12:34 -0500

    499. Instalando React Query DevTools

--- UpTask_BackEnd/src/server.ts
+++ UpTask_BackEnd/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import morgan from "morgan";
 import { corsConfig } from "./config/cors";
 import { connectDB } from "./config/db";
 import projectRoutes from "./routes/projectRoutes";
@@ -11,6 +12,10 @@ connectDB();
 const app = express(); // Generar la aplicacion de express
 app.use(cors(corsConfig)); // Se permite la conexión por cors con la configuración establecida
 
+// Loggin
+app.use(morgan("dev")); // Se permite la conexión por morgan con la configuración estable
+
+// Leer datos de formularios
 app.use(express.json()); // Habilita la lectura de JSON a través del body con ThunderClient
 
 // Routes
