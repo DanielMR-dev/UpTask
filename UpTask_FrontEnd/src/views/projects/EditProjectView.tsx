518ddda777fae181ac3fa000044ff718cefdc6dc
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-02 19:58:51 -0500

    495. Creando el Formulario para editar Proyectos

--- UpTask_FrontEnd/src/views/projects/EditProjectView.tsx
+++ UpTask_FrontEnd/src/views/projects/EditProjectView.tsx
@@ -1,21 +1,21 @@
 import { getProjectById } from "@/api/ProjectAPI";
+import EditProjectForm from "@/components/projects/EditProjectForm";
 import { useQuery } from "@tanstack/react-query";
-import { useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 
 export default function EditProjectView() {
     const params = useParams();
     const projectId = params.projectId!; // ! es usado para asegurarse de que el valor no sea null o undefined
     
     // Traer los proyectos desde la API
-    const { data, isLoading } = useQuery({
+    const { data, isLoading, isError } = useQuery({
         queryKey: ['editProject', projectId], // 
         queryFn: () => getProjectById(projectId), // Cuando se tienen que enviar parámetros a la funcion de queryFn se usa la sintáxis de callBack
         retry: false // Si se quiere que se vuelva a intentar la query si falla
     });
 
-    console.log(data);
+    if(isLoading) return 'Cargando...';
+    if(isError) return <Navigate to='/404' />
 
-    return (
-        <div>EditProjectView</div>
-    );
+    if(data) return <EditProjectForm />;
 };
