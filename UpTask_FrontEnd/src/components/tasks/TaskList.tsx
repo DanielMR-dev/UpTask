08b642277bc8295f6da8596df85bf8317b6159d1
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-04 19:37:32 -0500

    511. Aplicando diferentes colores a los estados

--- UpTask_FrontEnd/src/components/tasks/TaskList.tsx
+++ UpTask_FrontEnd/src/components/tasks/TaskList.tsx
@@ -27,6 +27,15 @@ const statusTranslations: {[key:string] : string} = { // Las keys y los valores 
     completed: 'Completado',
 }
 
+// Diccionario de colores
+const statusStyles: {[key:string] : string} = { // Las keys y los valores son strings
+    pending: 'border-t-slate-500', 
+    onHold: 'border-t-red-500',
+    inProgress: 'border-t-blue-500',
+    underReview: 'border-t-amber-500',
+    completed: 'border-t-emerald-500',
+}
+
 export default function TaskList({tasks} : TaskListProps) {
     // Agrupar las tareas en un objeto
     const groupedTasks = tasks.reduce((acc, task) => { // 
@@ -35,8 +44,6 @@ export default function TaskList({tasks} : TaskListProps) {
         return { ...acc, [task.status]: currentGroup };
     }, intialStatusGroups);
 
-    console.log(groupedTasks);
-
     return (
         <>
             <h2 className="text-5xl font-black my-10">Tareas</h2>
@@ -44,7 +51,9 @@ export default function TaskList({tasks} : TaskListProps) {
             <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
                 {Object.entries(groupedTasks).map(([status, tasks]) => (
                     <div key={status} className='min-w-[200px] 2xl:min-w-0 2xl:w-1/5'>
-                        <h3 className={`captalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8`}>{statusTranslations[status]}</h3>
+                        <h3 
+                            className={`captalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]}`}
+                        >{statusTranslations[status]}</h3>
                         <ul className='mt-5 space-y-5'>
                             {tasks.length === 0 ? (
                                 <li className="text-gray-500 text-center pt-3">No Hay tareas</li>
