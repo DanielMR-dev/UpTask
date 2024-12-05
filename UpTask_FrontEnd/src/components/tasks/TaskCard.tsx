01d9e034c9d55d6ab86c0be44330c88c806d2199
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-04 19:34:13 -0500

    510. Mostrando el Estado de la tarea a la que pertenecen

--- UpTask_FrontEnd/src/components/tasks/TaskCard.tsx
+++ UpTask_FrontEnd/src/components/tasks/TaskCard.tsx
@@ -28,7 +28,7 @@ export default function TaskCard({task} : TaskCardProps) {
                         enterTo="transform opacity-100 scale-100" leave="transition ease-in duration-75"
                         leaveFrom="transform opacity-100 scale-100" leaveTo="transform opacity-0 scale-95">
                         <Menu.Items
-                            className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                            className="absolute right-0 z-10 mt-2 w-52 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
                             <Menu.Item>
                                 <button type='button' className='block px-3 py-1 text-sm leading-6 text-gray-900'>
                                     Ver Tarea
--- UpTask_FrontEnd/src/components/tasks/TaskList.tsx
+++ UpTask_FrontEnd/src/components/tasks/TaskList.tsx
@@ -18,6 +18,15 @@ const intialStatusGroups : GroupedTasks = {
     completed: [],
 }
 
+// Diccionario para traducir los estados de las tareas
+const statusTranslations: {[key:string] : string} = { // Las keys y los valores son strings
+    pending: 'Pendiente', 
+    onHold: 'En Espera',
+    inProgress: 'En Progreso',
+    underReview: 'En Revision',
+    completed: 'Completado',
+}
+
 export default function TaskList({tasks} : TaskListProps) {
     // Agrupar las tareas en un objeto
     const groupedTasks = tasks.reduce((acc, task) => { // 
@@ -34,7 +43,8 @@ export default function TaskList({tasks} : TaskListProps) {
 
             <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
                 {Object.entries(groupedTasks).map(([status, tasks]) => (
-                    <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
+                    <div key={status} className='min-w-[200px] 2xl:min-w-0 2xl:w-1/5'>
+                        <h3 className={`captalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8`}>{statusTranslations[status]}</h3>
                         <ul className='mt-5 space-y-5'>
                             {tasks.length === 0 ? (
                                 <li className="text-gray-500 text-center pt-3">No Hay tareas</li>
