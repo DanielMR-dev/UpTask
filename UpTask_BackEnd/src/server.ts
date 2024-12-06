df5eed8ce448de4d6a3069d2942c0dbb7791879d
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-06 18:28:18 -0500

    528. Creando el Router para Autenticar

--- UpTask_BackEnd/src/server.ts
+++ UpTask_BackEnd/src/server.ts
@@ -4,13 +4,14 @@ import cors from "cors";
 import morgan from "morgan";
 import { corsConfig } from "./config/cors";
 import { connectDB } from "./config/db";
+import authRoutes from "./routes/authRoutes";
 import projectRoutes from "./routes/projectRoutes";
 
 dotenv.config();
 connectDB();
 
 const app = express(); // Generar la aplicacion de express
-app.use(cors(corsConfig)); // Se permite la conexión por cors con la configuración establecida
+// app.use(cors(corsConfig)); // Se permite la conexión por cors con la configuración establecida
 
 // Loggin
 app.use(morgan("dev")); // Se permite la conexión por morgan con la configuración estable
@@ -19,6 +20,7 @@ app.use(morgan("dev")); // Se permite la conexión por morgan con la configuraci
 app.use(express.json()); // Habilita la lectura de JSON a través del body con ThunderClient
 
 // Routes
-app.use('/api/projects', projectRoutes);
+app.use('/api/auth', authRoutes); // EndPoint para las autenticaciones
+app.use('/api/projects', projectRoutes); // EndPoint para los proyectos y tareas
 
 export default app
