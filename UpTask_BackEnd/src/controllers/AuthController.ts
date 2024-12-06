dd91ad7d6a28bfca87b14f0975d2653014905cac
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-06 18:57:57 -0500

    531. Algoritmo para crear cuentas

--- UpTask_BackEnd/src/controllers/AuthController.ts
+++ UpTask_BackEnd/src/controllers/AuthController.ts
@@ -1,9 +1,18 @@
 import { Request, Response } from "express";
+import User from "../models/User";
 
 export class AuthController {
 
     // CREATE
     static createAccount = async (req: Request, res: Response) => {
-        res.send('Desde /api/auth');
+        try {
+            const user = new User(req.body); // Se crea un nuevo usuario con los datos del body
+            await user.save(); // Se guarda el usuario en la base de datos
+
+            res.send('Cuenta creada correctamente, revisa tu email para confirmarla');
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ error: 'Server Error' });
+        }
     };
 };
