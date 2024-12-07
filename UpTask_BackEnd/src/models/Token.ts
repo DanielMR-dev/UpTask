c6ca4f2cad922048a440781f7ac1960be74c2688
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-06 19:26:55 -0500

    534. Creando el Modelo de Token

---
+++ UpTask_BackEnd/src/models/Token.ts
@@ -0,0 +1,29 @@
+import mongoose, { Schema, Document, Types } from "mongoose";
+
+// Definir la interface de User
+export interface IToken extends Document {
+    token: string;
+    user: Types.ObjectId;
+    createdAt: Date;
+};
+
+// Definir el Schema de Token
+const tokenSchema = new Schema({
+    token: { 
+        type: String, 
+        required: true 
+    },
+    user: { 
+        type: Types.ObjectId, 
+        ref: 'User' 
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+        expires: "10m" 
+    }
+});
+
+// Definir el Modelo para Mongoose
+const Token = mongoose.model<IToken>('Token', tokenSchema);
+export default Token;
