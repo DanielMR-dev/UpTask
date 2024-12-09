9174c236c33e1a81af29e9a1c93d0c340e59416a
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-08 19:19:04 -0500

    551. Enviando el nuevo codigo de confirmacion

--- UpTask_FrontEnd/src/api/AuthAPI.ts
+++ UpTask_FrontEnd/src/api/AuthAPI.ts
@@ -1,6 +1,6 @@
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
-import { ConfirmToken, UserRegistrationForm } from "../types";
+import { ConfirmToken, RequestConfirmationCodeForm, UserRegistrationForm } from "../types";
 
 // Crear Cuenta - POST
 export async function createAccount(formData : UserRegistrationForm) {
@@ -19,6 +19,19 @@ export async function createAccount(formData : UserRegistrationForm) {
 export async function confirmAccount(formData : ConfirmToken) {
     try {
         const url = '/auth/confirm-account'; // URL para crear cuenta
+        const { data } = await api.post<string>(url, formData); // Envía la solicitud POST a la API con la URL y los datos del formulario
+        return data;
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error); // Si el error es de axios y tiene una respuesta, se lanza un error con el mensaje de error
+        };
+    };  
+};
+
+// Confirmar Cuenta - POST
+export async function requestConfirmationCode(formData : RequestConfirmationCodeForm) {
+    try {
+        const url = '/auth/request-code'; // URL para crear cuenta
         const { data } = await api.post<string>(url, formData); // Envía la solicitud POST a la API con la URL y los datos del formulario
         return data;
     } catch (error) {
