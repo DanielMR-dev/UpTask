f5c2c2808815f86f36c1a810ce6003ba610acfeb
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-10 17:13:38 -0500

    566. Verificando el JWT en el backend

--- UpTask_BackEnd/src/middleware/auth.ts
+++ UpTask_BackEnd/src/middleware/auth.ts
@@ -1,7 +1,9 @@
 import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import User from "../models/User";
 
 // Middleware para revisar que un usuario este autenticado
-export const authenticate = (req: Request, res: Response, next: NextFunction) => {
+export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
     const bearer = req.headers.authorization; // Obtener el token del header Authorization
     if(!bearer) {
         const error = new Error("NO Autorizado");
@@ -10,6 +12,16 @@ export const authenticate = (req: Request, res: Response, next: NextFunction) =>
     };
 
     const [, token] = bearer.split(" "); // Obtener el token usando array destructuring
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET); // Verificar el token
+
+        if(typeof decoded === 'object' && decoded.id) { // 
+            const user = await User.findById(decoded.id); // Buscar el usuario en la base de datos
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Token No Válido' });
+    }
 
     next();
 };
