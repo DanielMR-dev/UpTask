a6a4aded7d75dd675b811342d06ae1295a71edd2
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-11 14:35:42 -0500

    574. Evitar repetir enviar el token con Interceptors

--- UpTask_FrontEnd/src/lib/axios.ts
+++ UpTask_FrontEnd/src/lib/axios.ts
@@ -4,4 +4,14 @@ const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL
 });
 
+// Antes de la petición HTTP
+api.interceptors.request.use( config => {
+    const token = localStorage.getItem('AUTH_TOKEN');
+    if(token) {
+        // Enviar la configuración con Headers y despues inyectar el Token
+        config.headers.Authorization = `Bearer ${token}`;
+    };
+    return config;
+});
+
 export default api;
