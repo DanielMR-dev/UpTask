0321b9ba203c617f8ba6578fd08bc2393112e6d7
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-12 21:14:30 -0500

    589. Agregando el usuario al proyecto

--- UpTask_FrontEnd/src/api/TeamAPI.ts
+++ UpTask_FrontEnd/src/api/TeamAPI.ts
@@ -1,14 +1,25 @@
 import { isAxiosError } from "axios";
 import api from "@/lib/axios";
-import { Project, TeamMemberForm } from "../types";
-
-
+import { Project, TeamMember, TeamMemberForm } from "../types";
 
 // Encontrar al usuario - POST
 export async function findUserByEmail({projectId, formData} : {projectId : Project['_id'], formData: TeamMemberForm}) {
     try {
         const url = `/projects/${projectId}/team/find`; // URL que se pasará a la API para la petición
         const { data } = await api.post(url, formData); // Se envía la petición POST por medio de la API
+        return data;
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error); // Si el error es de axios y tiene una respuesta, se lanza un error con el mensaje de error
+        };
+    };
+};
+
+// Agregar usuario al proyecto - POST
+export async function addUserToProject({projectId, id} : {projectId : Project['_id'], id: TeamMember['_id']}) {
+    try {
+        const url = `/projects/${projectId}/team`; // URL que se pasará a la API para la petición
+        const { data } = await api.post(url, {id}); // Se envía la petición POST por medio de la API
         return data;
     } catch (error) {
         if(isAxiosError(error) && error.response) {
--- UpTask_FrontEnd/src/components/team/SearchResult.tsx
+++ UpTask_FrontEnd/src/components/team/SearchResult.tsx
@@ -1,11 +1,37 @@
+import { addUserToProject } from "@/api/TeamAPI";
 import { TeamMember } from "@/types/index";
+import { useMutation } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
 type SearchResultProps = {
     user: TeamMember
 };
 
 export default function SearchResult({ user } : SearchResultProps) {
-    console.log(user);
+
+    const params = useParams();
+    const projectId = params.projectId!;
+    
+    // Función de mutación encargada de llamar la función de la API
+    const { mutate } = useMutation({
+        mutationFn: addUserToProject,
+        onError: (error) => {
+            toast.error(error.message);
+        },
+        onSuccess: (data) => {
+            toast.success(data);
+        }
+    });
+
+    // Función encargada de gestionar el botón de agregar un usuario al proyecto
+    const handleAddUserToProject = () => {
+        const data = { // Se crea el objeto con la información del usuario y el proyecto
+            projectId,
+            id: user._id
+        };
+        mutate(data); // Se llama a la función de mutación
+    };
 
     return (
         <>
@@ -14,6 +40,7 @@ export default function SearchResult({ user } : SearchResultProps) {
                 <p>{user.name}</p>
                 <button
                     className="text-purple-600 hover:bg-purple-200 px-10 py-3 font-bold cursor-pointer transition-colors shadow-md rounded-lg"
+                    onClick={handleAddUserToProject}
                 >Agregar al Proyecto</button>
             </div>
         </>
