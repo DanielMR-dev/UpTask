b9ebcab911eaad216eacd38881bc19054a1e9a4b
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-13 23:09:21 -0500

    597. Creando una función que revise si el usuario es manager

--- UpTask_FrontEnd/src/views/projects/ProjectDetailsView.tsx
+++ UpTask_FrontEnd/src/views/projects/ProjectDetailsView.tsx
@@ -3,10 +3,14 @@ import AddTaskModal from "@/components/tasks/AddTaskModal";
 import EditTaskData from "@/components/tasks/EditTaskData";
 import TaskList from "@/components/tasks/TaskList";
 import TaskModalDetails from "@/components/tasks/TaskModalDetails";
+import { useAuth } from "@/hooks/useAuth";
+import { isManager } from "@/utils/policies";
 import { useQuery } from "@tanstack/react-query";
 import { Link, Navigate, useNavigate, useParams } from "react-router-dom";
 
 export default function ProjectDetailsView() {
+
+    const { data: user, isLoading: authLoading } = useAuth();
 
     const navigate = useNavigate();
 
@@ -20,26 +24,28 @@ export default function ProjectDetailsView() {
         retry: false // Si se quiere que se vuelva a intentar la query si falla
     });
 
-    if(isLoading) return 'Cargando...';
+    if(isLoading && authLoading) return 'Cargando...';
     if(isError) return <Navigate to='/404' />
-    if(data) return (
+    if(data && user) return (
         <>
             <h1 className="text-5xl font-black">{data.projectName}</h1>
             <p className="text-2xl font-light text-gray-500 mt-5">{data.description}</p>
 
-            <nav className="mt-5 flex gap-3">
-                <button
-                    type="button"
-                    className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors shadow-lg rounded-lg"
-                    onClick={() => navigate(location.pathname + '?newTask=true')}
-                >Agregar Tarea</button>
+            {isManager(data.manager, user._id) && (
+                <nav className="mt-5 flex gap-3">
+                    <button
+                        type="button"
+                        className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors shadow-lg rounded-lg"
+                        onClick={() => navigate(location.pathname + '?newTask=true')}
+                    >Agregar Tarea</button>
 
-                <Link
-                    to={'team'}
-                    className="bg-fuchsia-600 hover:bg-fuchsia-700 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors shadow-lg rounded-lg"
-                >Colaboradores</Link>
-
-            </nav>
+                    <Link
+                        to={'team'}
+                        className="bg-fuchsia-600 hover:bg-fuchsia-700 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors shadow-lg rounded-lg"
+                    >Colaboradores</Link>
+                </nav>
+            )}
+            
             <TaskList
                 tasks={data.tasks}
             />
