08b5784f51e66db82b3d8671ffb7b0170ea30fe3
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-16 09:02:31 -0500

    627. Multiples Mutaciones en React Query

--- UpTask_FrontEnd/src/api/AuthAPI.ts
+++ UpTask_FrontEnd/src/api/AuthAPI.ts
@@ -1,6 +1,6 @@
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
-import { ConfirmToken, ForgotPasswordForm, NewPasswordForm, RequestConfirmationCodeForm, User, UserLoginForm, UserRegistrationForm, userSchema } from "../types";
+import { CheckPasswordForm, ConfirmToken, ForgotPasswordForm, NewPasswordForm, RequestConfirmationCodeForm, User, UserLoginForm, UserRegistrationForm, userSchema } from "../types";
 
 // Crear Cuenta - POST
 export async function createAccount(formData : UserRegistrationForm) {
@@ -97,7 +97,7 @@ export async function updatePasswordWithToken({formData, token} : {formData: New
 // Obtener el Usuario
 export async function getUser() {
     try {
-        const { data } = await api.get<User>('/auth/user');
+        const { data } = await api.get<User>('/auth/user'); // Hacer la solicitud GET a la API para obtener el usuario
         const response = userSchema.safeParse(data);
         if(response.success) {
             return response.data
@@ -109,3 +109,16 @@ export async function getUser() {
         };
     }  
 };
+
+// Revisar el Password
+export async function checkPassword(formData : CheckPasswordForm) {
+    try {
+        const url = '/auth/check-password'; // URL para verificar el password
+        const { data } = await api.post<string>(url, formData); // Hacer la solicitud POST a la API con la URL y los datos del formulario
+        return data;
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error); // Si el error es de axios y tiene una respuesta, se lanza un error con el mensaje de error
+        };
+    }
+}
