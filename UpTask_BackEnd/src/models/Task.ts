e2b379d11df547a08252bc7bb1e238686e43073e
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-17 11:02:43 -0500

    641. Eliminar Notas cuando se elimina una tarea

--- UpTask_BackEnd/src/models/Task.ts
+++ UpTask_BackEnd/src/models/Task.ts
@@ -1,4 +1,5 @@
 import mongoose,{ Schema, Document, Types } from "mongoose";
+import Note from "./Note";
 
 const taskStatus = {
     PENDING: 'pending',
@@ -65,6 +66,13 @@ export const TaskSchema: Schema = new Schema({
         },
     ],
 }, {timestamps: true});
+
+// Middleware
+TaskSchema.pre('deleteOne', {document: true}, async function() {
+    const taskId = this._id; // Obtiene el ID de la Tarea a eliminar
+    if(!taskId) return;
+    await Note.deleteMany({task: taskId}); // Elimina las Notas asociadas a la Tarea
+});
 
 // Definir el Modelo para Mongoose
 const Task = mongoose.model<ITask>('Task', TaskSchema);
